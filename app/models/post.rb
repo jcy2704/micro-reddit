626d0d0a5f0af9990b7edc9d0ab97fdd7e79f94d
Steven Jack Chung <stevenjchung12@gmail.com>
2020-11-06 12:11:47 -0500

    Update User and Post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :title, uniqueness: true, presence: true
+  validates :body, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 6..20 }
 end
