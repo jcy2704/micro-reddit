2a0ddca9c20954b6eb29c89d1dd0d754802a67e4
Steven Jack Chung <stevenjchung12@gmail.com>
2020-11-09 15:31:33 -0500

    Add comment model and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..20 }
+  validates :email, presence: true, uniqueness: true
 end
